280d111b43d15d0c9438be04e5e56f1088523c53
kevinazhou150 <35386714+kevinazhou150@users.noreply.github.com>
2018-06-24 23:48:21 -0400

    Update AddSpacing.java

--- AddSpacing.java
+++ AddSpacing.java
@@ -2,6 +2,11 @@ package hats;
 
 import java.util.Scanner;
 
+/* When you get a strategy, it simply outputs everything on one line.
+ * This just makes it spaced better (although still not that much better.
+ * It is very possible that this class will be converted to something
+ * that can process the sets that we find using LineOfSagesSetFinder.java.
+ */
 public class AddSpacing {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
