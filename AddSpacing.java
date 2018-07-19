f3aa929591b1533a3924fd8d48f8d33712767f2c
kevinazhou150 <35386714+kevinazhou150@users.noreply.github.com>
2018-07-18 21:11:33 -0400

    Update AddSpacing.java

--- AddSpacing.java
+++ AddSpacing.java
@@ -1,20 +1,37 @@
 package hats;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/* When you get a strategy, it simply outputs everything on one line.
- * This just makes it spaced better (although still not that much better.
- * It is very possible that this class will be converted to something
- * that can process the sets that we find using LineOfSagesSetFinder.java.
- */
 public class AddSpacing {
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		String str = reader.nextLine();
-		str = str.replaceAll("],", "]\n");
-
-		System.out.println(str);
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner reader = new Scanner(new File("3p5c.txt"));
+		while(reader.hasNext()) {
+			
+			String str = reader.nextLine();
+			
+			/* Data processing methods lie ahead. 
+			 * Choose which ones you want to apply to your string.
+			 */
+			
+			//str = str.replaceAll("],", "]\n"); //1. adds spacing
+			
+			int index = 8;
+			str = str.substring(str.indexOf("["));
+			
+			String lasts = "";
+			while(index < str.length()) {
+				lasts += (str.charAt(index));
+				index += 11;
+				if((index-8) % 44 == 0)
+					lasts += "\n";
+			}
+			
+			
+			System.out.println(lasts);
+			
+		}
 		reader.close();
 	}
 }
